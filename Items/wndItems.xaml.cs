15dea9db4223a9ee5b936929679f772d35d307e2
Austin Duran <austin.duran39@gmail.com>
2020-07-20 15:16:40 -0600

    Add project files.

---
+++ Items/wndItems.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+/// <summary>
+/// @author: Joe Dimmick, Ankit Dhamala, Austin Duran
+/// @assignment: Group Project
+/// </summary>
+namespace GroupProject.Items
+{
+    /// <summary>
+    /// Interaction logic for wndItems.xaml
+    /// </summary>
+    public partial class wndItems : Window
+    {
+        public wndItems()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+        /// <summary>
+        /// HandleError shows the error to the user and saves to root directory
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C://Error.txt", Environment.NewLine +
+                                             "HandleError Excpetion: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Main/clsMainLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+/// <summary>
+/// @author: Joe Dimmick, Ankit Dhamala, Austin Duran
+/// @assignment: Group Project
+/// </summary>
+namespace GroupProject.Main
+{
+    class clsMainLogic
+    {
+        /// <summary>
+        /// HandleError shows the error to the user and saves to root directory
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C://Error.txt", Environment.NewLine +
+                                             "HandleError Excpetion: " + ex.Message);
+            }
+        }
+    }
+
+}
