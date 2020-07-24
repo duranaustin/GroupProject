8d9b4744e685a5ac59e19419da4fc2cdd6b4958c
Joe <33135630+JoeDimmick@users.noreply.github.com>
2020-07-24 16:31:56 -0600

    Added Functionality to menu items. Windows will now open.

--- Main/wndMain.xaml.cs
+++ Main/wndMain.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using GroupProject.Items;
+using GroupProject.Search;
 
 namespace GroupProject
 {
@@ -21,7 +23,16 @@ namespace GroupProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Attributes
+        /// <summary>
+        /// Window Items
+        /// </summary>
         public wndItems itemsWindow;
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Constructor for main window and application entry point.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -31,5 +42,92 @@ namespace GroupProject
             //itemsWindow.Show(); //temporary for austin's development
             
         }
+        /// <summary>
+        /// Handles the Exit menu item being clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {               //this is reflection
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //Code to be ran no matter what. Closing connnections to dbs/web
+            }
+        }
+        /// <summary>
+        /// Handles the Item Inventory Menu item click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ItemInventory_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                wndItems wndItems = new wndItems();
+                wndItems.ShowDialog();
+            }
+            catch (Exception ex)
+            {               //this is reflection
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //Code to be ran no matter what. Closing connnections to dbs/web
+            }
+        }
+        /// <summary>
+        /// Handles the Search Invoices menu item click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchInvoices_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                wndSearch wndSearch = new wndSearch();
+                wndSearch.ShowDialog();
+            }
+            catch (Exception ex)
+            {               //this is reflection
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //Code to be ran no matter what. Closing connnections to dbs/web
+            }
+        }
+        /// <summary>
+        /// Exception handler.
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {//would write to a file or database here.
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+                                            "HandleError Exception: " + ex.Message);
+            }
+        }
+
+        #endregion
+
+
     }
 }
