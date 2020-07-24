1f0e885ef44cd3923fb7fb49377a71d6ef0c0b83
Joe <33135630+JoeDimmick@users.noreply.github.com>
2020-07-24 17:10:17 -0600

    Added SQL queries to Insert new Invoice, Delete an Invoice, Update Invoice.

--- Main/clsMainSQL.cs
+++ Main/clsMainSQL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,6 +29,59 @@ namespace GroupProject.Main
             {
                 System.IO.File.AppendAllText("C://Error.txt", Environment.NewLine +
                                              "HandleError Excpetion: " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// SQL query that inserts an Invoice into the database.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="total"></param>
+        /// <returns></returns>
+        public string New_Invoice(string date, string total)
+        {
+            try
+            {
+                return $"INSERT INTO Invoices (InvoiceDate, TotalCost VALUES ({date.ToString()}, {total})";
+            }
+            catch (Exception ex)
+            {                       //this is reflection for exception handling
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// SQL query that updates the given invoice number with the given total.
+        /// </summary>
+        /// <param name="invoiceNumber"></param>
+        /// <param name="total"></param>
+        /// <returns></returns>
+        public string Update_Invoice_Total(string invoiceNumber, string total)
+        {
+            try
+            {
+                return $"UPDATE Invoices SET TotalCost = {total} WHERE InvoiceNum = {invoiceNumber}";
+            }
+            catch (Exception ex)
+            {                       //this is reflection for exception handling
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// SQL query that deletes the passed invoice from the database.
+        /// </summary>
+        /// <param name="invoiceNumber"></param>
+        /// <returns></returns>
+        public string Delete_Invoice(string invoiceNumber)
+        {
+            try
+            {
+                return $"DELETE FROM Invoices WHERE InvoiceNum = {invoiceNumber}";
+            }
+            catch (Exception ex)
+            {                       //this is reflection for exception handling
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
             }
         }
     }
