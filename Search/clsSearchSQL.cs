8b43e16d539da452a4bbfabce390394be8cde1a1
Joe <33135630+JoeDimmick@users.noreply.github.com>
2020-08-09 21:18:50 -0600

    Fixed Invoice dropdown box bug

--- Search/clsSearchSQL.cs
+++ Search/clsSearchSQL.cs
@@ -76,7 +76,7 @@ namespace GroupProject.Search
                 //Create the SQL statement to extract the Invoices
                 string sSQL = "SELECT* FROM Invoices";
 
-                ds = ds = db.ExecuteSQLStatement(sSQL, ref iRet);
+                ds = db.ExecuteSQLStatement(sSQL, ref iRet);
                 
                 //Loop through the data and create an Invoice class
                 for (int i = 0; i < iRet; i++)
@@ -109,13 +109,13 @@ namespace GroupProject.Search
                 //Create the SQL statement to extract the Invoices
                 string sSQL = "SELECT TotalCost FROM Invoices ORDER BY TotalCost ASC";
 
-                ds = ds = db.ExecuteSQLStatement(sSQL, ref iRet);
+                ds = db.ExecuteSQLStatement(sSQL, ref iRet);
                 //Loop through the data and create an Invoice class
                 for (int i = 0; i < iRet; i++)
                 {
                     list.Add(new clsInvoices
                     {
-                        TotalCost = ds.Tables[0].Rows[i][2].ToString()
+                        TotalCost = ds.Tables[0].Rows[i][0].ToString()
                     });
                 }
                 return list;
@@ -142,7 +142,7 @@ namespace GroupProject.Search
                 //Create the SQL statement to extract the Invoices
                 string sSQL = "SELECT* FROM Invoices";
 
-                ds = ds = db.ExecuteSQLStatement(sSQL, ref iRet);
+                ds = db.ExecuteSQLStatement(sSQL, ref iRet);
                 //Loop through the data and create an Invoice class
                 for (int i = 0; i < iRet; i++)
                 {
